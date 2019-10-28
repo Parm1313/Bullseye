e1b5044f1d47a7f6dc20dfdde8113c88172ff72f
Parm1313 <56977103+Parm1313@users.noreply.github.com>
2019-10-28 15:01:37 -0400

    Add files via upload

---
+++ Bullseye2/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Bullseye2
+//
+//  Created by MacStudent on 2019-10-28.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var currentValue: Int = 50
+    var targetValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = Int(slider.value)
+    }
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue) and the targeted value is \(targetValue)"
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        startNewRound()
+        
+    }
+    
+
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+        print("The value of the slider is now: \(slider.value)")
+        
+        print("The value of the slider is now: \(currentValue)")
+    
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
+}
+
